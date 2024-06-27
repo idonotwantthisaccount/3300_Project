4482442c8f1273761e94e1eb933f60ab7bc5663b
tommyjgman <123404493+tommyjgman@users.noreply.github.com>
2024-06-27 01:42:34 -0600

    Update views.py
    Proper CC BY-NC-SA 4.0 licensing comment.

--- mountains/views.py
+++ mountains/views.py
@@ -1,10 +1,10 @@
-#3300_Project (c) by Brian Richardson, Ethan Briggs, Jordan Camden, and Thomas Gherna
+# 3300_Project (c) by Brian Richardson, Ethan Briggs, Jordan Camden, and Thomas Gherna
 #
-#3300_Project is licensed under a
-#Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+# 3300_Project is licensed under a
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 #
-#You should have received a copy of the license along with this
-#work. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
+# You should have received a copy of the license along with this
+# work. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
 from django.shortcuts import render
 import json, datetime
 import urllib.request
