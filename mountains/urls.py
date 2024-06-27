c3ac7e9e603d58200c13040855770701f4c9abef
tommyjgman <123404493+tommyjgman@users.noreply.github.com>
2024-06-27 01:43:20 -0600

    Update urls.py
    Proper CC BY-NC-SA 4.0 licensing comment.

--- mountains/urls.py
+++ mountains/urls.py
@@ -1,10 +1,10 @@
 # 3300_Project (c) by Brian Richardson, Ethan Briggs, Jordan Camden, and Thomas Gherna
 #
 # 3300_Project is licensed under a
-# Creative Commons Attribution 4.0 International License.
+# Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 #
 # You should have received a copy of the license along with this
-# work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+# work. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
 from django.urls import path
 from . import views
 
