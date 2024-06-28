cd06fe36ffb5f72bccf1e6a3a05c4116f4d10b38
tommyjgman <123404493+tommyjgman@users.noreply.github.com>
2024-06-28 09:44:46 -0600

    Update urls.py
    Updated Licensing Comment.

--- coloradosnow/urls.py
+++ coloradosnow/urls.py
@@ -1,10 +1,11 @@
-# 3300_Project (c) by Brian Richardson, Ethan Briggs, Jordan Camden, and Thomas Gherna
+# 3300_Project
+# (c) by Brian Richardson, Ethan Briggs, Jordan Camden, and Thomas Gherna
+# This file is part of the 3300_Project and is licensed under the GNU Affero General Public License (AGPLv3).
+# You may obtain a copy of the AGPLv3 license at <https://www.gnu.org/licenses/agpl-3.0.en.html>.
 #
-# 3300_Project is licensed under a
-# Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
-#
-# You should have received a copy of the license along with this
-# work. If not, see <https://creativecommons.org/licenses/by-nc-sa/4.0/>.
+# This project uses weather data from Open-Meteo (<https://open-meteo.com/>),
+# licensed under the Creative Commons Attribution 4.0 International License (CC BY 4.0).
+# You can view the license at <https://creativecommons.org/licenses/by/4.0/>.
 """
 URL configuration for coloradosnow project.
 
